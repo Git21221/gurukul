75a0e5ecf8c0722cda10081bc89ed17b664768ae
Saikat Das <saikatdas40g@gmail.com>
2025-02-22 14:50:49 +0530

    fix: update branding loading logic and improve code formatting in App component

--- gurukul/gurukul-apps/client/brand-app/brand/src/App.jsx
+++ gurukul/gurukul-apps/client/brand-app/brand/src/App.jsx
@@ -1,28 +1,30 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [branding, setBranding] = useState(null);
 
   useEffect(() => {
-        // Extract brand name dynamically from URL
-        const brandFolder = window.location.pathname.split("/")[1] || "default";
-    
-        // Construct full URL to branding.json
-        const brandingUrl = `/${brandFolder}/branding.json`;
-    
-        fetch(brandingUrl)
-          .then((response) => response.json())
-          .then((data) => setBranding(data))
-          .catch((error) => console.error("Error loading branding:", error));
-      }, []);
+    // Extract brand name dynamically from URL
+    const brandFolder = window.location.pathname.split("/")[1] || "default";
+
+    // Construct full URL to branding.json
+    const brandingUrl = `/${brandFolder}/branding.json`;
+
+    fetch(brandingUrl)
+      .then((response) => response.json())
+      .then((data) => setBranding(data))
+      .catch((error) => console.error("Error loading branding:", error));
+  }, [branding]);
   return (
     <>
       <p>our logo {branding?.logo}</p>
-      <h1 style={{color: branding?.color}}>Welcome to our brand's official website. {branding?.name}</h1>
+      <h1 style={{ color: branding?.color }}>
+        Welcome to our brand's official website. {branding?.name}
+      </h1>
       <div className="content">
-        <button onClick={() => alert('Button Clicked!')}>Click Me</button>
+        <button onClick={() => alert("Button Clicked!")}>Click Me</button>
       </div>
       <div>
         <p>Count: {count}</p>
@@ -30,7 +32,7 @@ function App() {
         <button onClick={() => setCount(0)}>Reset</button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
