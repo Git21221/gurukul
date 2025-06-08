7ab3553a18aa6519752c77bd0577a32ebb21f1b8
Saikat Das <saikatdas40g@gmail.com>
2025-06-08 16:03:51 +0530

    chore: update ESLint configuration and upgrade react-hooks plugin to version 5.1.0

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,30 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
+  js.configs.recommended,
   {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    files: ["**/*.jsx", "**/*.tsx"],
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+    },
     languageOptions: {
       parserOptions: {
-        ecmaVersion: 2021,
+        ecmaVersion: 2022,
         sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
     rules: {
-      semi: ["error", "always"],
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
 ];
