72face28e0c1b857388540e2ba3ad496f0a5282a
Saikat Das <saikatdas40g@gmail.com>
2025-06-08 21:42:51 +0530

    feat: Update Sidebar component to use NavLink for navigation and add paths to sidebar items

--- gurukul/gurukul-apps/client/brand-app/brand/src/components/sidebar/Sidebar.jsx
+++ gurukul/gurukul-apps/client/brand-app/brand/src/components/sidebar/Sidebar.jsx
@@ -1,17 +1,23 @@
+import { NavLink } from 'react-router-dom';
 import { sidebarBrandList } from '../../data/SidebarData';
+import { useSelector } from 'react-redux';
 export const Sidebar = () => {
+  const { userRole } = useSelector((state) => state.auth);
   return (
     <div className="sidebar flex flex-col max-w-[265px] font-medium text-[15px]">
-      {sidebarBrandList.map(({ title, component: Item }, index) => (
-        <div
-          className="sidebar-item flex items-center gap-7 bg-[var(--brand-course-box)] p-5 rounded-tr-full rounded-br-full"
+      {sidebarBrandList.map(({ title, component: Item, path }, index) => (
+        <NavLink
+          to={`${userRole}/${path}`}
+          className={({ isActive }) =>
+            `sidebar-item flex items-center gap-7 ${isActive ? 'bg-[var(--brand-course-box)]' : ''} p-5 rounded-tr-full rounded-br-full`
+          }
           key={index}
         >
           <div className="sidebar-icon">
             <Item />
           </div>
           <span className="sidebar-title">{title}</span>
-        </div>
+        </NavLink>
       ))}
     </div>
   );
--- gurukul/gurukul-apps/client/brand-app/brand/src/data/SidebarData.js
+++ gurukul/gurukul-apps/client/brand-app/brand/src/data/SidebarData.js
@@ -9,17 +9,21 @@ export const sidebarBrandList = [
   {
     title: 'Home',
     component: HomeLogo,
+    path: 'home',
   },
   {
     title: 'Courses',
     component: CourseLogo,
+    path: 'courses',
   },
   {
     title: 'Articles',
     component: ArticleLogo,
+    path: 'articles',
   },
   {
     title: 'Updates',
     component: UpdatesLogo,
+    path: 'updates',
   },
 ];
